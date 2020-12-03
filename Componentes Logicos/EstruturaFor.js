864019a69aa28d218de77e8d2db24688e7ddc209
Gabrie2121 <gabriel.miranda041200@gmail.com>
2020-12-02 23:27:08 -0300

    Estrutura For Simples

---
+++ Componentes Logicos/EstruturaFor.js
@@ -0,0 +1,9 @@
+//for classico
+//ao inves de i++ vc pode colocar i+=10 ou i-=10
+//e fazer a adicao de como vc quiser
+const frutas = ['Maça', 'Pera', 'Uva', 'Morango', 'Pessego'];
+for (let i = 0; i < frutas.length; i++) {
+    //const par = i%2 === 0;
+    //console.log(i, par===true?'par':'impar');    
+    console.log(frutas[i])
+}
