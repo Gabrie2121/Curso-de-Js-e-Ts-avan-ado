16aebe4e4418a9e473f10a0d7e83f0d517672cfa
Gabriel Miranda <63580608+Gabrie2121@users.noreply.github.com>
2020-12-08 19:58:08 -0300

    Ensinado While, falta DoWhile

---
+++ Componentes Logicos/While&DoWhile.js
@@ -0,0 +1,23 @@
+// const nome = 'Luiz';
+// let control = 0;
+
+// da para usar lenght
+//  while(control<10){
+//      console.log(control)
+//      control++
+//  }
+const random = (min,max)=>{
+    const r = Math.random()*(max-min)+min;
+    return Math.ceil(r);
+}
+
+let rand = random(1, 50);
+console.log(rand);
+let i = 0
+//ele só sai do loop quando o argumento der diferente
+while(rand !== 10){
+    rand = random(1,50)
+    console.log('Não')
+    i++
+    console.log(i)
+}
