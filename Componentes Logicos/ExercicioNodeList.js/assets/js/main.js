e813015ebe17956ab90f704516b2a36c271fd050
Gabriel Miranda <63580608+Gabrie2121@users.noreply.github.com>
2020-12-08 18:37:19 -0300

    Finalizado Desafio NodeList e correto

--- Componentes Logicos/ExercicioNodeList.js/assets/js/main.js
+++ Componentes Logicos/ExercicioNodeList.js/assets/js/main.js
@@ -9,5 +9,6 @@ const backgroundColorBody = estilosBody.backgroundColor;
 console.log(backgroundColorBody)
 
 for(let p of ps){
-    p.style.color = backgroundColorBody
+    p.style.backgroundColor= backgroundColorBody
+    p.style.color = 'white'
 }
