1e18d2f294fab61fc9dcfe8790f3c8ef5e9d11e6
Gabrie2121 <gabriel.miranda041200@gmail.com>
2020-12-14 23:24:51 -0300

    Finalizado Aula de Intervalos e TimeOut

--- Componentes Logicos/setIntervalos.js
+++ Componentes Logicos/setIntervalos.js
@@ -1,1 +1,28 @@
- 
+ function mostraHora(){
+     let data = new Date();
+     return data.toLocaleTimeString('pt-BR',{
+         hour12: false
+     });
+ }
+//  function funcaodoInteval(){
+//      console.log(mostraHora())
+//  }
+
+// //passa a referencia da funcao sem as parenteses
+// //funcao,parametros em MS
+// setInterval(funcaodoInteval,1000);
+
+//usar função anonima ajuda com o codigo limpo
+//funciona com ou sem variavel pra chamar
+const timer = setInterval(() => {
+    console.log(mostraHora())
+}, 1000);
+
+//set TimeOut
+setTimeout(()=>{
+    clearInterval(timer)
+},10000)
+//da pra colocar coisas dentro e usar como timeOut
+setTimeout(()=>{
+    console.log('erro')
+},10000)
