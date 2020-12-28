931e89250d4745298d3c2e767e56ce7c89c68f88
Gabriel Miranda <63580608+Gabrie2121@users.noreply.github.com>
2020-12-27 21:49:00 -0300

    Finalizado aula de Closures

---
+++ Funções Avançadas/Closures.js
@@ -0,0 +1,11 @@
+//closures é relacionado a escopo lexico
+//closures é a habilidade da função referente ao escopo lexico
+function criaOutra(){
+    const nome = 'Luiz';
+    return function(){
+        return nome;
+    }
+}
+const funcao = criaOutra();
+//console.dir === console.log no v8
+console.dir(funcao)
