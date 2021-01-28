0b9c122024221d45448230c8e586de77275596b0
Gabriel Miranda <63580608+Gabrie2121@users.noreply.github.com>
2021-01-27 23:04:44 -0300

    Finalizado junção dos 3 metodos reduce, map, filter

---
+++ Arrays avançado/ReducMapFilt.js
@@ -0,0 +1,12 @@
+// Os 3 juntos
+
+// tudo sobre os pares
+// Filtrar pares
+// Dobrar os valores
+// reduzir(somar tudo)
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+
+const numerosPares = numeros.filter(valor => valor % 2 == 0).map(valor => valor * 2).reduce((ac, value) => ac + value)
+
+
+console.log(numerosPares)
